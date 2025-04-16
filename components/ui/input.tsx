52f4cb00b4d618a607a979cdb3b0fc3117fd470a
kytas999 <kytasevichvlad@gmail.com>
2025-04-16 09:53:48 +0300

    input color

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
       <input
         type={type}
         className={cn(
-          'flex w-full h-11 rounded-md border border-input text-darker-gray outline-none bg-white p-4 text-base ring-offset-transparent file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex w-full h-11 rounded-md border border-input placeholder:text-darker-gray text-neutral-black outline-none bg-white p-4 text-base ring-offset-transparent file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
