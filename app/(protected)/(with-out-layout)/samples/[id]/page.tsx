b55ef578706ae806006522c0572e6b3b58a45572
kytas999 <kytasevichvlad@gmail.com>
2025-04-23 22:31:27 +0300

    fix build issue

--- app/(protected)/(with-out-layout)/samples/[id]/page.tsx
+++ app/(protected)/(with-out-layout)/samples/[id]/page.tsx
@@ -1,7 +1,7 @@
-import { getSampleById } from '@/lib/sample';
+import { getSampleById } from "@/lib/sample";
 
-import { SampleInputs, SampleActionButtons } from '../components/sample-meta';
-import { Skeleton } from '@/components/ui/skeleton';
+import { SampleInputs, SampleActionButtons } from "../components/sample-meta";
+import { Skeleton } from "@/components/ui/skeleton";
 export default async function SampleDetailPage({
   params,
 }: {
@@ -9,24 +9,16 @@ export default async function SampleDetailPage({
 }) {
   const { data: sample, message, error } = await getSampleById(params.id);
 
-  if (error) {
+  if (error || !sample) {
     throw new Error(message || error);
   }
 
   return (
     <>
       <div className="p-6 absolute inset-0">
-        <SampleInputs
-          data={{
-            assignmentTitle: 'test data ',
-            courseTitle: 'test data ',
-            studentName: 'test data ',
-            grade: 10,
-            date: new Date().toLocaleDateString(),
-          }}
-        />
+        <SampleInputs sample={sample} />
         <Skeleton className="w-full h-[90vh]" />
-        <SampleActionButtons sample={sample!} />
+        <SampleActionButtons sample={sample} />
       </div>
     </>
   );
