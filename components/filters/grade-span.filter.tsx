28d845022e08fd19485220fca5065cba3e813c33
kytas999 <kytasevichvlad@gmail.com>
2025-09-11 00:02:32 +0300

    Revert "fix: update GradeSpanFilter to use mapGrade for value generation; adjust GradeFilter value to include 'Grade' prefix"
    This reverts commit 8de51b7f1cffc6efca0837d3a069e1ed77f78104.

--- components/filters/grade-span.filter.tsx
+++ components/filters/grade-span.filter.tsx
@@ -1,18 +1,14 @@
-import { DEFAULT_FILTERS_KEYS, FILTER_SEPARATOR_FOR_MULTIPLE_VALUES } from '@/constants/filter';
+import { DEFAULT_FILTERS_KEYS } from '@/constants/filter';
 import { BaseFilter } from './base';
 
 interface GradeFilterProps {
   slug?: string;
 }
 
-function mapGrade(grades: number[]): string {
-  return grades.map(value => `Grade ${value}`).join(FILTER_SEPARATOR_FOR_MULTIPLE_VALUES);
-}
-
 const GRADES = [
-  { label: 'Elementary school', value: mapGrade([1,2,3,4,5]) },
-  { label: 'Middle school', value: mapGrade([6,7,8,9,10]) },
-  { label: 'High school', value: mapGrade([11,12]) },
+  { label: 'Elementary school', value: '1,2,3,4,5' },
+  { label: 'Middle school', value: '6,7,8,9,10' },
+  { label: 'High school', value: '11,12' },
 ];
 
 export function GradeSpanFilter({ slug = DEFAULT_FILTERS_KEYS.GRADE }: GradeFilterProps) {
--- components/filters/grade.filter.tsx
+++ components/filters/grade.filter.tsx
@@ -7,7 +7,7 @@ interface GradeFilterProps {
 
 const GRADES = Array.from({ length: 12 }, (_, index) => ({
   label: `Grade ${index + 1}`,
-  value: `Grade ${index + 1}`,
+  value: `${index + 1}`,
 }));
 
 export function GradeFilter({ slug = DEFAULT_FILTERS_KEYS.GRADE }: GradeFilterProps) {
